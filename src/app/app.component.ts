a5c52f92187915f62dd33aced0440935ebf45050
Erinc Karatas <1380275+ekaratas@users.noreply.github.com>
2019-11-05 23:53:48 +0300

    v1.0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,24 +4,72 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { Plugins } from '@capacitor/core';
+import { LoadingController } from '@ionic/angular';
+const { Network } = Plugins;
+const { Toast } = Plugins;
+
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  private loader: any;
+  private loaderActive: boolean = false;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private loadingController: LoadingController
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      let handler = Network.addListener('networkStatusChange', (status) => {
+        console.log(status);
+        if (!status.connected) {
+          this.showLoader('İnternet Bağlantınız Yok !');
+        } 
+
+        if (status.connected) {
+          this.dismissLoader();
+        }
+        if (status.connectionType === 'wifi') {
+          this.show('Güvenliğiniz için Hücresel Ağa geçiniz !');
+        }
+      });
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
   }
+
+  async show(msg) {
+    await Toast.show({
+      text: msg
+    });
+  }
+
+  async showLoader(msg) {
+    this.loaderActive = true;
+    this.loader = await this.loadingController.create({
+      message: msg,
+      spinner: null,
+      translucent: true
+    });
+    await this.loader.present();
+    }
+
+async dismissLoader() {
+      if (this.loaderActive === true) {
+        await this.loader.dismiss();
+      }
+      this.loaderActive = false;
+      }
+
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import { Plugins } from '@capacitor/core';
+
+const { Clipboard } = Plugins;
+const { Toast } = Plugins;
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +12,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  deger: string = "Merhaba Elmadağ !";
+
   constructor() {}
+
+  async show(msg) {
+    await Toast.show({
+      text: msg
+    });
+  }
+
+  async yaz() {
+    Clipboard.write({
+      string: this.deger,
+    });
+  }
+
+  async oku() {
+
+    let str = await Clipboard.read({
+      type: "string"
+    });
+    console.log('Panodaki Değer : ', str.value);
+    this.show('Panodaki Değer : ' + str.value);
+
+  }
 
 }
